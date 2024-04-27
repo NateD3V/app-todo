45604da838a010f1b48ab2cf28e9d4dec0cb075f
Nathan <adm.nate@hotmail.com>
2024-04-27 20:47:12 -0300

    registered service worker

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-const registerServiceWorker = async () => {
-  if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register(
-        "./service-worker.js",
-        {
-          scope: "/",
-        }
-      );
-      if (registration.installing) {
-        console.log("Service worker installing");
-      } else if (registration.waiting) {
-        console.log("Service worker installed");
-      } else if (registration.active) {
-        console.log("Service worker active");
-      }
-    } catch (error) {
-      console.error(`Registration failed with ${error}`);
-    }
-  }
-};
-
-// …
-
-registerServiceWorker();
-
 // seleções
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
